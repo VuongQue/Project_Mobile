764805d1182bb4cbfa395f276fdd8d4fc00e4d7c
Alro127 <nhuttan373@gmail.com>
2025-04-09 21:07:30 +0700

    thêm backend

---
+++ s_parking_api/src/main/java/com/example/s_parking/SParkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.s_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SParkingApplication.class, args);
+	}
+
+}
---
+++ s_parking_api/src/test/java/com/example/s_parking/SParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
